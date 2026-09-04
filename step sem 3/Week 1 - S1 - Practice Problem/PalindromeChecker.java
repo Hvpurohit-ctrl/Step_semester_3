9ce7d24037c3ad9e98fc973dcd75fcc9ca52fd47
Harshvardhan Purohit <purohitharshvardhan74@gmail.com>
2026-09-04 23:27:41 +0530

    practice problem

---
+++ step sem 3/Week 1 - S1 - Practice Problem/PalindromeChecker.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    static boolean isPalindromeIterative(String text) {
+        int start = 0;
+        int end = text.length() - 1;
+
+        while (start < end) {
+            if (text.charAt(start) != text.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    static boolean isPalindromeRecursive(String text) {
+        if (text.length() <= 1) {
+            return true;
+        }
+
+        if (text.charAt(0) != text.charAt(text.length() - 1)) {
+            return false;
+        }
+
+        return isPalindromeRecursive(text.substring(1, text.length() - 1));
+    }
+
+    static boolean isPalindromeArrayReversal(String text) {
+        char[] arr = text.toCharArray();
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            char temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+
+        String reversed = new String(arr);
+
+        return text.equals(reversed);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        boolean iterative = isPalindromeIterative(text);
+        boolean recursive = isPalindromeRecursive(text);
+        boolean arrayReversal = isPalindromeArrayReversal(text);
+
+        System.out.println("Iterative: " +
+                (iterative ? "Palindrome" : "Not Palindrome"));
+
+        System.out.println("Recursive: " +
+                (recursive ? "Palindrome" : "Not Palindrome"));
+
+        System.out.println("Array Reversal: " +
+                (arrayReversal ? "Palindrome" : "Not Palindrome"));
+
+        sc.close();
+    }
+}
